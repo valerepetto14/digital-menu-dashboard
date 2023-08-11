72d323fef866b579f6a00ef798ec743e8c409c77
valerepetto14 <valentinrepetto12345@gmail.com>
2023-08-11 11:55:00 -0300

    start to build sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import { RiDashboardLine } from "react-icons/ri";
+import {
+  RiDashboardLine,
+  RiBriefcaseLine,
+  RiCalendar2Line,
+  RiMessage2Line,
+  RiLogoutBoxRLine,
+} from "react-icons/ri";
 
 function App() {
   return (
@@ -8,16 +14,51 @@ function App() {
           <h1 className="uppercase font-bold tracking-[4px]">Tu logo</h1>
         </div>
         {/* MENU */}
-        <nav>
-          <ul>
-            <li>
-              <a href="#" className="flex items-center p-8">
-                <RiDashboardLine className="mr-4" />
-                Dashboard
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <div className="h-[800px] flex flex-col justify-between">
+          <nav>
+            <ul>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 hover:bg-purple-700 p-4 text-gray-400 font-semibold hover:text-white rounded-lg transition-colors"
+                >
+                  <RiDashboardLine className="mr-4" />
+                  Dashboard
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 hover:bg-purple-700 p-4 text-gray-400 font-semibold hover:text-white rounded-lg transition-colors"
+                >
+                  <RiBriefcaseLine className="mr-4" />
+                  Jobs
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 hover:bg-purple-700 p-4 text-gray-400 font-semibold hover:text-white rounded-lg transition-colors"
+                >
+                  <RiCalendar2Line className="mr-4" />
+                  Calendar
+                </a>
+                <a
+                  href="#"
+                  className="flex items-center gap-4 hover:bg-purple-700 p-4 text-gray-400 font-semibold hover:text-white rounded-lg transition-colors"
+                >
+                  <RiMessage2Line className="mr-4" />
+                  Messages
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="flex">
+            <a
+              href="#"
+              className="flex items-center gap-4 hover:bg-purple-700 p-4 text-gray-400 font-semibold hover:text-white rounded-lg transition-colors"
+            >
+              <RiLogoutBoxRLine className="mr-4" />
+              Logout
+            </a>
+          </div>
+        </div>
       </div>
       <div className="col-span-5">
         <div className="flex flex-col items-center">

093f4d594fef02c97f1ac4639ee26afdedf04925
valerepetto14 <valentinrepetto12345@gmail.com>
2023-08-13 22:48:19 -0300

    fix image size

--- src/components/products/card.jsx
+++ src/components/products/card.jsx
@@ -5,7 +5,11 @@ const ProductCard = ({ product }) => {
   return (
     <div className="bg-white p-8 mb-3 rounded-2xl flex flex-col md:flex-row border-2 border-white hover:border-blue-800 shadow-lg">
       <div className="w-full md:w-[10%] mb-10 md:mb-0 flex justify-center items-center rounded-xl">
-        <img className="rounded-xl w-[80%]" src={product.image} alt="image" />
+        <img
+          className="rounded-xl md:w-[80%]"
+          src={product.image}
+          alt="image"
+        />
       </div>
       <div className="w-full md:w-[60%] flex flex-col gap-2 mb-10 md:mb-0">
         <div className="flex flex-col md:flex-row gap-2">
